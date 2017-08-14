55dca9ecf3642f451b97c6c8d20a171fc20bd0c3
jaehoon.Jang <wkdwo16@naver.com>
2017-08-15 01:52:21 +0900

    asd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cors = require('cors');
 var app = express();
 
 var mongoose    = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/soccer');
+mongoose.connect('mongodb://localhost:27017/soccer', { useMongoClient : true });
 
 var index = require('./routes/index');
 var users = require('./routes/users');
